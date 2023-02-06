b0bb03d2f4a1f81533cbd8c0278836282d340418
Ayano <ayanotakky6@gmail.com>
2023-02-06 18:54:16 +0900

    修正

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className='border-b flex items-center h-14 px-4'>
+      <h1>Population Change</h1>
+    </header>
+  );
+};
+
+export default Header;
