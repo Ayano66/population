37fc02f5ea12c8df04a62e1e2905fa3b7bbc0299
Ayano <ayanotakky6@gmail.com>
2023-02-06 13:21:53 +0900

    グラフ表示

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -2,11 +2,57 @@ import React from 'react';
 import HighchartsReact from 'highcharts-react-official';
 import Highcharts from 'highcharts';
 
+const Styles: { [key: string]: React.CSSProperties } = {
+  graph: {
+    padding: '12px',
+  },
+};
+
 interface Props {
-  graphData: {};
+  population: { prefName: string; data: { year: number; value: number }[] }[];
 }
-const Graph: React.FC<Props> = ({ graphData }) => {
-  return <HighchartsReact highCharts={Highcharts} option={graphData}></HighchartsReact>;
+
+const Graph: React.FC<Props> = ({ population }) => {
+  let series: Highcharts.SeriesOptionsType[] = [];
+  let categories = [];
+
+  for (let p of population) {
+    let data = [];
+
+    for (let pd of p.data) {
+      data.push(pd.value);
+      categories.push(String(pd.year));
+    }
+    series.push({
+      type: 'line',
+      name: p.prefName,
+      data: data,
+    });
+  }
+
+  const options: Highcharts.Options = {
+    title: {
+      text: '総人口推移',
+    },
+    xAxis: {
+      title: {
+        text: '年度',
+      },
+    },
+    yAxis: {
+      title: {
+        text: '人口数',
+      },
+      categories: categories,
+    },
+    series: series.length === 0 ? [{ type: 'line', name: '都道府県名', data: [] }] : series,
+  };
+
+  return (
+    <div style={Styles.graph}>
+      <HighchartsReact highcharts={Highcharts} options={options} />
+    </div>
+  );
 };
 
 export default Graph;
